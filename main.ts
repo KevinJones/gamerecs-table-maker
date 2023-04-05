0b59ea293bc2adeea63b73d154c170fa0cae3ecf
Kevin Jones <kevin.chris.jones@gmail.com>
2023-04-04 20:21:26 -0400

    Adds initial implementation of game rec table action.

--- main.ts
+++ main.ts
@@ -1,13 +1,16 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile, TFolder } from 'obsidian';
+import { getAPI } from 'obsidian-dataview'
 
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+	recsFolderPath: string;
+	tableTemplatePath: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	recsFolderPath: 'gamerecs',
+	tableTemplatePath: 'templates/Recs Table Template.md'
 }
 
 export default class MyPlugin extends Plugin {
@@ -16,66 +19,65 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
 			id: 'sample-editor-command',
 			name: 'Sample editor command',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+				const vault = this.app.vault
+
+				// testing: insert the first file in the list.
+				// const files = vault.getMarkdownFiles()
+				// console.log(files)
+
+				const dv = getAPI(this.app)
+				if (dv === undefined)
+				{
+					new Notice('Could not get the Dataview API.')
+					return;
+				}
+
+				const data = dv.pages('"gamerecs"')
+					.sort(b => b["release-date"])
+					.dateformat()
+					.map(b => [b.file.link, b.developer, b.publisher, b["release-date"], b.oneliner, b["play-today"]]);
+				// console.log(JSON.stringify(data.array()));
+				
+				const table = dv.markdownTable(["Title", "Developer", "Publisher", "Release Date", "Desc", "Play"], data)
+
+				const recFolder = vault.getAbstractFileByPath(this.settings.recsFolderPath);
+				const templateFile = vault.getAbstractFileByPath(this.settings.tableTemplatePath);
+
+				if (recFolder instanceof TFolder && templateFile instanceof TFile) {
+					// console.log(templateFile)
+					// console.log(recFolder.children)
+
+					// const rowFmt = "<tr> </tr><tr> </tr><tr> </tr><tr> </tr><tr> </tr><tr> </tr>"
+
+					const tem = vault.cachedRead(templateFile)
+					console.log(tem)
+
+					const outputText = "assign output text here"
+					editor.replaceSelection(table)
+				}
+				else
+				{
+					if (!(recFolder instanceof TFolder)) {
+						new Notice("Could not open recs folder at " + this.settings.recsFolderPath)
 					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+					if (!(recFolder instanceof TFile)) {
+						new Notice("Could not open recommendation table template at" + this.settings.tableTemplatePath)
+					}
+
+					return
 				}
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -88,22 +90,6 @@ export default class MyPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
 	}
 }
 
@@ -123,14 +109,24 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Template path')
+			.setDesc('The markdown file that serves as a template for your recommendation data.')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter a file')
+				.setValue(this.plugin.settings.tableTemplatePath)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.tableTemplatePath = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Recommendations folder path')
+			.setDesc('The folder that contains the items to include in the table.')
+			.addText(text => text
+				.setPlaceholder('Enter a path')
+				.setValue(this.plugin.settings.recsFolderPath)
+				.onChange(async (value) => {
+					this.plugin.settings.recsFolderPath = value;
 					await this.plugin.saveSettings();
 				}));
 	}
